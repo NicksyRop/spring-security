291707f147fe09e9c58c35942b3b87fb253d636e
Nickson Kipkorir <nnkipkorir@dtbafrica.com>
2024-01-27 10:47:24 +0300

    added method security

--- src/main/java/com/security/controller/ProductController.java
+++ src/main/java/com/security/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.security.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class ProductController {
     }
 
     @GetMapping("/auth")
+    @PreAuthorize("hasAnyAuthority('USER')")
     public String auth(){
         return "Authenticate to products";
     }
